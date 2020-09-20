764752a41550298f3442098d8a5053f2f88d74b7
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-09-20 21:24:43 +0300

    update(Posts): moved data into an array

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,14 +3,19 @@ import Post from './Post/Post';
 import style from './Posts.module.css';
 
 const Posts = () => {
+  let postsData = [
+    { id: 1, message: "Hi, how are you?" },
+    { id: 2, message: "Its my fists post!" },
+  ]
+  let postsElements = postsData.map(post => <Post message={post.message} />);
+
   return (
     <div className={style.posts}>
       <div className={style.create}>
         <textarea></textarea>
         <button>Send</button>
       </div>
-      <Post message='Hi, how are you?' />
-      <Post message='Its my fists post!' />
+      { postsElements }
     </div>
   )
 }
