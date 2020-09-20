a0092babd00400102bf9516938d80f5e9059f47d
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-09-20 21:24:29 +0300

    update(Dialogs): moved data into an array

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,17 +4,27 @@ import style from './Dialogs.module.css';
 import Message from './Message/Message';
 
 const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Dimich" },
+    { id: 2, name: "Alexey" },
+    { id: 3, name: "Igor" },
+    { id: 4, name: "Sergey" }
+  ]
+  let dialogsElements = dialogsData.map(dialog => <Dialog name={dialog.name} id={dialog.id} />);
+
+  let messagesData = [
+    { id: 1, message: "Hello!" },
+    { id: 2, message: "Yo!" },
+  ]
+  let messagesElements = messagesData.map(message => <Message text={message.message} />);
+
   return (
     <div className={style.dialogsContainer}>
       <div className={style.dialogs}>
-        <Dialog name="Dimich" id="1"/>
-        <Dialog name="Alexey" id="2"/>
-        <Dialog name="Igor" id="3"/>
-        <Dialog name="Sergey" id="4"/>
+        {dialogsElements}
       </div>
       <div className={style.messages}>
-        <Message text="Hello!"/>
-        <Message text="Yo!"/>
+        {messagesElements}
       </div>
 
     </div>
