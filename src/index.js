6ae2e4d4eee323fb5b125bd464b45db12c0614f2
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-09-23 08:11:16 +0300

    update(*): delete render.js, added observer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import {rerenderEntireTree} from './render';
+import state, {subscribe} from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {addPost} from './redux/state';
+import {updateNewPostText} from './redux/state';
 
-
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from './../render';
+let rerenderEntireTree = () => {
+  console.log('state changed');
+}
 
 
 let state = {
@@ -29,7 +31,7 @@ let state = {
     ]
   }
 }
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 3,
     message: state.profilePage.newPostText
@@ -39,9 +41,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 }
 
 export default state;
