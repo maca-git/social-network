3019ec83d9cdfd8c38b9564aaabca5f4ca4b80f9
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-09-24 07:30:34 +0300

    update(index.js, state.js): refactor

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addPost} from './redux/state';
-import {updateNewPostText} from './redux/state';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,54 @@
-let rerenderEntireTree = () => {
-  console.log('state changed');
-}
-
-
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "Hi, how are you?" },
-      { id: 2, message: "Its my fists post!" },
-    ],
-    newPostText: ''
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "Hi, how are you?" },
+        { id: 2, message: "Its my fists post!" },
+      ],
+      newPostText: ''
+    },
+    messagesPage: {
+      dialogsData: [
+        { id: 1, name: "Dimich" },
+        { id: 2, name: "Alexey" },
+        { id: 3, name: "Igor" },
+        { id: 4, name: "Sergey" },
+      ],
+      messagesData: [
+        { id: 1, message: "Hello!" },
+        { id: 2, message: "Yo!" },
+      ]
+    },
+    navbarState: {
+      friendsData: [
+        { id: 1, name: "Anna", avatar: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/128771571/original/c540edc5830213c6de3243d97061c3f12cdc2d3b/design-a-digital-icon-of-your-roblox-character.png"},
+        { id: 2, name: "Ivan", avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuCwcYJIQnwXyQcyN8DdYfM0UDCUd_gprKEQ&usqp=CAU"},
+        { id: 3, name: "Igor", avatar: "https://pm1.narvii.com/6544/9e5275d11654e09a0786ea750c4c9f64ccee5a58_hq.jpg"}
+      ]
+    }
   },
-  messagesPage: {
-    dialogsData: [
-      { id: 1, name: "Dimich" },
-      { id: 2, name: "Alexey" },
-      { id: 3, name: "Igor" },
-      { id: 4, name: "Sergey" },
-    ],
-    messagesData: [
-      { id: 1, message: "Hello!" },
-      { id: 2, message: "Yo!" },
-    ]
+  getState() {
+    return this._state;
   },
-  navbarState: {
-    friendsData: [
-      { id: 1, name: "Anna", avatar: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/128771571/original/c540edc5830213c6de3243d97061c3f12cdc2d3b/design-a-digital-icon-of-your-roblox-character.png"},
-      { id: 2, name: "Ivan", avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuCwcYJIQnwXyQcyN8DdYfM0UDCUd_gprKEQ&usqp=CAU"},
-      { id: 3, name: "Igor", avatar: "https://pm1.narvii.com/6544/9e5275d11654e09a0786ea750c4c9f64ccee5a58_hq.jpg"}
-    ]
+  _callSubscriber() {
+    console.log('state changed');
+  },
+  addPost() {
+    let newPost = {
+      id: 3,
+      message: this._state.profilePage.newPostText
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   }
 }
-export const addPost = () => {
-  let newPost = {
-    id: 3,
-    message: state.profilePage.newPostText
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
 
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
