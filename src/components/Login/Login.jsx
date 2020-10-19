45296a763d41e50f1f71c62694a3c0ec5db779ed
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-10-19 07:24:52 +0300

    update(*): connected redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Field from 'redux-form/lib/Field';
+import reduxForm from 'redux-form/lib/reduxForm';
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field component={"input"} name={"login"} placeholder={"Login"}/>
+      </div>
+      <div>
+        <Field component={"input"} name={"password"} type={"password"} placeholder={"Password"}/>
+      </div>
+      <div>
+        <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
+const LoginReduxForm = reduxForm({
+  form: 'login'
+})(LoginForm)
 
 const Login = (props) => {
+  const onSubmit = (formData) => {
+
+  }
   return (
     <div>
-      Login
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
   );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import dialogReducer from "./dialogs-reduser";
 import navbarReducer from "./navbar-reduser";
 import profileReducer from "./profile-reduser";
 import usersReducer from "./users-reduser";
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   messagesPage: dialogReducer,
   navbarState: navbarReducer,
   usersPage: usersReducer,
-  auth: authReduser
+  auth: authReduser,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
