092f4e65776d32b4aeff8c605482dc43a885c146
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-11-03 12:34:30 +0200

    update(*): deploy to github

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const AppContainer = compose(connect(mapStateToProps, {initializeApp}))(App);
 const MainApp = (props) => {
   return (
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer/>
       </Provider>
